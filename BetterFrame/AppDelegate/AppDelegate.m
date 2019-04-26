f60b30cbc084cfe3824cc6fee3a8b7c745a5089a
Ian_iOS <302601530@qq.com>
2019-04-26 14:16:26 +0800

    测试

--- BetterFrame/AppDelegate/AppDelegate.m
+++ BetterFrame/AppDelegate/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"测试");
+    NSLog(@"测试1");
     return YES;
 }
 
